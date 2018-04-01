8da899ddad27c0cff8ab99d56f8b89020ea206aa
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-04-01 17:40:48 -0300

    add acript for the analysis of code quality and coverage.

---
+++ Jbmp/scripts/sonarcloud.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+workspace="/home/max/git/Jbmp/Jbmp/"
+export JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+export M2_HOME="/opt/apache-maven-3.5.2"
+export PATH="$PATH:${JAVA_HOME}${M2_HOME}"
+
+echo -e "\e[1;34m-----------------------------------------\e[0m"
+echo -e "\e[1;34m.      Maximiliano Correa Jbmp           \e[0m"
+echo -e "\e[1;34m-----------------------------------------\e[0m"
+
+echo -e "\e[1;37m------- Enveronment Veriables -----------\e[0m"
+echo -e "\e[1;33m       Workspace --- $workspace          \e[0m"
+echo -e "\e[1;33m       JAVA_HOME --- $JAVA_HOME          \e[0m"
+echo -e "\e[1;33m       M2_HOME   --- $M2_HOME            \e[0m"
+echo -e "\e[1;37m-----------------------------------------\e[0m"
+
+cd $workspace
+
+echo -e "\e[1;37m------- Preparing the coverage ----------\e[0m"
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --settings settings.xml
+echo -e "\e[1;37m-----------------------------------------\e[0m"
+
+echo -e "\e[1;37m- Analyzing and uploading to sonarcloud -\e[0m"
+mvn sonar:sonar \ -Dsonar.organization=maxicorrea-github \ -Dsonar.host.url=https://sonarcloud.io \ -Dsonar.login=30c467aae36c8a0c34108159a0732c630c7c21d7 --settings settings.xml
+echo -e "\e[1;37m-----------------------------------------\e[0m"
