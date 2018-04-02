437c3efe27ff1158a579583ee6b5519a4d5be05e
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-04-02 01:16:43 -0300

    create MotionFrame class.
    It is an object in charge of the events of the window and closing tags
    and minimize custom.

---
+++ Jbmp/src/main/java/com/maxicorrea/jbmp/views/utils/TextLabels.java
@@ -0,0 +1,26 @@
+package com.maxicorrea.jbmp.views.utils;
+
+public class TextLabels {
+
+  public static final String APP_TITLE =  "________JBMP________";
+  public static final String TITLE_ESSENTIAL = "Essentials";
+  public static final String OPEN = "open";
+  public static final String SAVE = "save";
+
+  public static final String TITLE_FILTER = "Filters";
+  public static final String GRAYSCALE = "grayscale";
+  public static final String SEPIA = "sepia";
+  public static final String NEGATIVE = "negative";
+
+  public static final String TITLE_FLIP = "Flips";
+  public static final String VERTICAL = "vertical";
+  public static final String HORIZONTAL = "horizontal";
+  public static final String LEFT = "left";
+  public static final String RIGHT = "right";
+  public static final String RESET = "reset";
+
+  private TextLabels() {
+    throw new AssertionError("TextLabels is a utility class.");
+  }
+
+}
