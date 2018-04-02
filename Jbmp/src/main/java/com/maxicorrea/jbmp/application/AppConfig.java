3a95e05fb506e02050ce68161c08031a97a0cd54
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-04-01 22:31:29 -0300

    config urls

---
+++ Jbmp/src/main/java/com/maxicorrea/jbmp/application/AppConfig.java
@@ -0,0 +1,36 @@
+package com.maxicorrea.jbmp.application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class AppConfig {
+
+  private static final Map<String, String> URL = new HashMap<>();
+
+  static {
+    try {
+      Properties properties = new Properties();
+      char sp = File.separatorChar;
+      properties.loadFromXML(
+          new FileInputStream("src" + sp + "main" + sp + "resources" + sp + "application.xml"));
+      for (Object key : properties.keySet()) {
+        URL.put((String) key, properties.getProperty((String) key));
+      }
+    } catch (IOException e) {
+      System.exit(-1);
+    }
+  }
+  
+  public static final String url(String key) {
+    return URL.get(key);
+  }
+
+  private AppConfig() {
+    throw new AssertionError();
+  }
+  
+}
--- Jbmp/src/main/java/com/maxicorrea/jbmp/views/utils/IconLocations.java
+++ Jbmp/src/main/java/com/maxicorrea/jbmp/views/utils/IconLocations.java
@@ -1,19 +1,20 @@
 package com.maxicorrea.jbmp.views.utils;
 
+import com.maxicorrea.jbmp.application.AppConfig;
+
 public class IconLocations {
 
-  public static final String GRAYSCALE_PATH = "/images/negative.png";
-  public static final String NEGATIVE_PATH = "/images/negative.png";
-  public static final String SEPIA_PATH = "/images/negative.png";
-  public static final String ARROW_DOWN = "/images/arrow_down.png";
-  public static final String ARROW_UP = "/images/arrow_up.png";
-  public static final String CLOSE = "/images/close.png";
-  public static final String MINI = "/images/mini.png";
-  public static final String FAVICON = "/images/favicon.png";
-  public static final String ROTATE_LEFT = "/images/rotate_left.png";
-  public static final String ROTATE_RIGHT = "/images/rotate_right.png";
-  
-
+  public static final String GRAYSCALE_PATH = AppConfig.url("brightness");
+  public static final String NEGATIVE_PATH = AppConfig.url("reset");
+  public static final String SEPIA_PATH = AppConfig.url("negative");
+  public static final String ARROW_DOWN = AppConfig.url("arrowdown");
+  public static final String ARROW_UP = AppConfig.url("arrowup");
+  public static final String CLOSE = AppConfig.url("close");
+  public static final String MINI = AppConfig.url("mini");
+  public static final String FAVICON = AppConfig.url("favicon");
+  public static final String ROTATE_LEFT = AppConfig.url("rotateleft");
+  public static final String ROTATE_RIGHT = AppConfig.url("rotateright");
+ 
   private IconLocations() {
     throw new AssertionError("IconLocations is a utility class.");
   }
