2c1d4c183d4dd939c368fc6e8811cb0c70db31e2
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-05-20 15:35:51 -0300

    applied nimbus look and feel.

--- Jbmp/src/main/java/com/maxicorrea/jbmp/application/Application.java
+++ Jbmp/src/main/java/com/maxicorrea/jbmp/application/Application.java
@@ -1,23 +1,31 @@
 package com.maxicorrea.jbmp.application;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import com.maxicorrea.jbmp.controllers.EssentialsController;
 import com.maxicorrea.jbmp.controllers.OperationController;
 import com.maxicorrea.jbmp.models.core.Editor;
-import com.maxicorrea.jbmp.views.ImageView;
 import com.maxicorrea.jbmp.views.EditorView;
+import com.maxicorrea.jbmp.views.ImageView;
 
 public class Application {
 
   public static void main(String[] args) {
     SwingUtilities.invokeLater(() -> {
-      EditorView view =  new EditorView(new ImageView());
-      initializeApp( new Editor(), view);
+      try {
+        UIManager.setLookAndFeel(new NimbusLookAndFeel()); 
+      } catch (UnsupportedLookAndFeelException e) {
+        System.err.println(e);
+      }
+      EditorView view = new EditorView(new ImageView());
+      initializeApp(new Editor(), view);
       view.show();
     });
   }
-  
-  static void initializeApp(Editor model , EditorView view) {
+
+  static void initializeApp(Editor model, EditorView view) {
     model.addObserver(view.getImageView());
     new OperationController(view, model);
     new EssentialsController(view, model);
